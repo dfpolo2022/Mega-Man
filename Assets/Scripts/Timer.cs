f5fefd0aad710cfe2117ae4565d31af14710a618
Daniel Felipe Polo Vitola <dfpolo@uninorte.edu.co>
2022-03-18 11:26:34 -0500

    class update

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -6,18 +6,30 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     public Text UItexto;
+    public static Timer instanciar;
     
     private int cont = 0;
+    public bool timerOn = true;
 
     private void Awake()
     {
-        InvokeRepeating("Cronometro", 0f, 1f);
+        instanciar = this;
+
+        if (timerOn)
+        {
+            InvokeRepeating("Cronometro", 0f, 1f);
+        }
+        else
+        {
+            finTiempo();
+        }
+        
     }
 
     void Cronometro()
     {
         
-            if (cont == 60)
+            if (cont == 59)
             {
                 cont = 0;
             }
@@ -33,6 +45,11 @@ public class Timer : MonoBehaviour
             {
                 UItexto.text = "0" + cont.ToString();
             }        
+    }
+
+    public void finTiempo()
+    {
+        timerOn = false;
     }
 
 }
